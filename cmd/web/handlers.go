20399609070167da9cff93097b14d59a14b4c327
Kevin He <kevin66he@hotmail.com>
2024-07-23 15:02:05 -0400

    Added Note Handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World"))
+}
+
+func (app *application) noteView(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %s...", id)
+}
+
+func (app *application) noteCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating a note"))
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,7 +9,13 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
 	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/note/view", app.noteView)
+	router.HandlerFunc(http.MethodPost, "/note/create", app.noteCreate)
 
 	return router
 }
