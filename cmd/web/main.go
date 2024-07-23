d2d2ae006c0baff3c0ac72b5d8cb9fdb0a559754
Kevin He <kevin66he@hotmail.com>
2024-07-23 14:23:50 -0400

    Starting up Server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	debug bool
+}
 
 func main() {
 	fmt.Println("Hello World")
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	app := &application{
+		debug: false,
+	}
+
+	srv := &http.Server{
+		Addr:         *addr,
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Println("Starting server on :4000")
+	err := srv.ListenAndServe()
+	log.Fatal(err)
 }
